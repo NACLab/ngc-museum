1c291f2347677ae582c8089a3e7842e977277a76
Alexander Ororbia <ago@hal3.cs.rit.edu>
2024-03-30 12:46:42 -0400

    init commit of dc model

--- exhibits/diehl_cook_snn/README.md
+++ exhibits/diehl_cook_snn/README.md
@@ -8,3 +8,52 @@ and studied in:
 Diehl, Peter U., and Matthew Cook. "Unsupervised learning of digit recognition
 using spike-timing-dependent plasticity." Frontiers in computational
 neuroscience 9 (2015): 99.
+
+## Description
+
+This model is effectively made up of three layers -- a sensory input layer made up 
+of Poisson encoding neuronal cells, a hidden layer of excitatory leaky integrators, 
+and another layer of inhibitory leaky integrators. The sensory layer connects to 
+excitatory layer with a synaptic cable that is adapted with traced-based 
+spike-timing-dependent plasticity. The excitatory layer connects to the inhibitory 
+layer with a fixed identity matrix/cable (all excitatory neurons map one-to-one to 
+inhibitory ones) while the inhibitory layers connects to the excitatory layer 
+via a fixed, negatively scaled hollow matrix/cable. 
+
+The dynamics that result from the above structure is a form of sensory input-driven 
+leaky integrator dynamics that are recurrently inhibited by the laterally-wired 
+inhibitory leaky integrators.
+
+<i>Task</i>: This model engages in unsupervised representation learning and simply 
+learns sparse spike train patterns that correlate with different input digit patterns 
+sampled from the MNIST database.
+
+## Hyperparameters
+
+This model requires the following hyperparameters, tuned to produce results much akin 
+to that of the original Diehl and Cook model:
+
+```
+// Note: resistance scale values set to 1
+tau_m_e = 100.500896468 ms (excitatory membrane time constant)
+tau_m_i = 100.500896468 ms (inhibitory membrane time constant)
+tau_tr= 20. ms (trace time constant)
+// STDP hyper-parameters
+Aplus = 1e-2 ## LTD learning rate (STDP); nu1
+Aminus = 1e-4 ## LTD learning rate (STDP); nu0
+w_norm = 78.4 ## L1 norm constraint
+# excitatory-to-inhibitory scale factor
+factor = 22.5
+# inhibitory-to-excitatory scale factor
+factor = -120
+# excitatory dynamics (with adaptive threshold)
+thr = -52 mv
+v_rest = -65 mv
+v_reset = -60 mv 
+tau_theta = 1e7
+theta_plus = 0.05
+# inhibitory dynamics (adaptive threshold disabled)
+thr = -40 mv 
+v_rest = -60 mv
+v_reset = -45 mv
+```
---
+++ exhibits/diehl_cook_snn/train_model.py
@@ -0,0 +1,69 @@
+from snn_model import DC_SNN_Model as Model
+from jax import numpy as jnp, random
+import sys
+
+## bring in ngc-learn analysis tools
+from ngclearn.utils.viz.raster import create_raster_plot
+from ngclearn.utils.patch_utils import generate_patch_set
+
+_X = jnp.load("../data/baby_mnist/babyX.npy")
+_Y = jnp.load("../data/baby_mnist/babyY.npy")
+n_batches = _X.shape[0]
+
+viz_mod = 1
+n_samp_mod = 100 #50
+mb_size = 1
+n_iter = 10 #1
+patch_shape = (28, 28)
+in_dim = patch_shape[0] * patch_shape[1]
+
+T = 200 #250 # num discrete time steps to simulate
+dt = 1.
+
+dkey = random.PRNGKey(1234)
+#dkey, *subkeys = random.split(dkey, 10)
+
+################################################################################
+## Create model
+model = Model(dkey, in_dim=in_dim, T=T, dt=dt)
+################################################################################
+
+print("****")
+model.viz_receptive_fields(fname="start", field_shape=patch_shape)
+
+x_ref = _X[0:1,:]
+_S = model.process(obs=x_ref, adapt_synapses=False, collect_spike_train=True)
+create_raster_plot(jnp.concatenate(_S,axis=0).T, tag="{}".format(0),
+                   plot_fname="{}/raster/z1e_raster_i.jpg".format(model.exp_dir))
+
+for i in range(n_iter):
+    dkey, *subkeys = random.split(dkey, 2)
+    ptrs = random.permutation(subkeys[0],_X.shape[0])
+    X = _X[ptrs,:]
+    Y = _Y[ptrs,:]
+
+    n_samps_seen = 0
+    for j in range(n_batches):
+        idx = j #j % 2 # 1
+        Xb = X[idx: idx + mb_size,:]
+
+        _S = model.process(obs=Xb, adapt_synapses=True)
+
+        n_samps_seen += Xb.shape[0]
+        print("\r Seen {} images...".format(n_samps_seen), end="")
+        if n_samps_seen % n_samp_mod == 0:
+            print()
+            model.viz_receptive_fields(fname="tmp", field_shape=patch_shape)
+    if (i+1) % viz_mod == 0:
+        print()
+        model.viz_receptive_fields(fname=str(i+1), field_shape=patch_shape)
+print()
+print("****")
+
+model.viz_receptive_fields(fname="final", field_shape=patch_shape)
+model.save_to_disk() # save final state of synapses to disk
+
+x_ref = _X[0:1,:]
+_S = model.process(obs=x_ref, adapt_synapses=False, collect_spike_train=True)
+create_raster_plot(jnp.concatenate(_S,axis=0).T, tag="{}".format(0),
+                   plot_fname="{}/raster/z1e_raster_f.jpg".format(model.exp_dir))
