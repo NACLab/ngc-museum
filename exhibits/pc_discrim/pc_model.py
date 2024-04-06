7e0c4da14677f6eb4e05675ddb9ec5ad9af2d6a8
Alexander Ororbia <ago@hal3.cs.rit.edu>
2024-04-05 20:11:37 -0400

    got very basic pcn prototype to work

--- exhibits/pc_discrim/pc_model.py
+++ exhibits/pc_discrim/pc_model.py
@@ -3,12 +3,12 @@ from ngclearn.utils.io_utils import makedir
 from ngclearn.utils.viz.raster import create_raster_plot
 from ngclearn.utils.viz.synapse_plot import visualize
 from jax import numpy as jnp, random
-import time
+import time, sys
 
-class DC_SNN_Model():
+class PCN():
 
-    def __init__(self, dkey, in_dim, hid1_dim=128, hid2_dim=64, T=200, dt=0.1, exp_dir="exp",
-                 model_name="snn_stdp", **kwargs):
+    def __init__(self, dkey, in_dim, out_dim, hid1_dim=128, hid2_dim=64, T=10,
+                 dt=1., tau_m=10., act_fx = "tanh", exp_dir="exp", model_name="pc_disc", **kwargs):
         self.exp_dir = exp_dir
         makedir(exp_dir)
         makedir(exp_dir + "/filters")
@@ -16,37 +16,45 @@ class DC_SNN_Model():
 
         dkey, *subkeys = random.split(dkey, 10)
 
+        self.T = T
+        self.dt = dt
+        #tau_m = 10.
+        #act_fx = "tanh"
         optim_type = "adam"
         eta = 0.002
+        wlb = -0.3
+        wub = 0.3
 
         ## set up model with layers of neuronal cells
         model = Controller()
+        ## construct core generative model
         z0 = model.add_component("rate", name="z0", n_units=in_dim, tau_m=0.,
                                  act_fx="identity", leakRate=0., key=subkeys[0])
-        e0 = model.add_component("error", name="e0", n_units=in_dim)
         z1 = model.add_component("rate", name="z1", n_units=hid1_dim, tau_m=tau_m,
-                                 act_fx="tanh", leakRate=0., key=subkeys[1])
+                                 act_fx=act_fx, leakRate=0., key=subkeys[1])
         e1 = model.add_component("error", name="e1", n_units=hid1_dim)
         z2 = model.add_component("rate", name="z2", n_units=hid2_dim, tau_m=tau_m,
-                                 act_fx="tanh", leakRate=0., key=subkeys[2])
-        z3 = model.add_component("rate", name="z3", n_units=in_dim, tau_m=0.,
+                                 act_fx=act_fx, leakRate=0., key=subkeys[2])
+        e2 = model.add_component("error", name="e2", n_units=hid2_dim)
+        z3 = model.add_component("rate", name="z3", n_units=out_dim, tau_m=0.,
                                  act_fx="identity", leakRate=0., key=subkeys[3])
+        e3 = model.add_component("error", name="e3", n_units=out_dim)
         ### set up generative/forward synapses
         W1 = model.add_component("hebbian", name="W1", shape=(in_dim, hid1_dim),
-                                 eta=eta, wInit=("uniform", -0.3, 0.3), w_bound=0.,
+                                 eta=eta, wInit=("uniform", wlb, wub), w_bound=0.,
                                  optim_type=optim_type, signVal=-1., key=subkeys[4])
         W2 = model.add_component("hebbian", name="W2", shape=(hid1_dim, hid2_dim),
-                                 eta=eta, wInit=("uniform", -0.3, 0.3), w_bound=0.,
+                                 eta=eta, wInit=("uniform", wlb, wub), w_bound=0.,
                                  optim_type=optim_type, signVal=-1., key=subkeys[5])
         W3 = model.add_component("hebbian", name="W3", shape=(hid2_dim, out_dim),
-                                 eta=eta, wInit=("uniform", -0.3, 0.3), w_bound=0.,
+                                 eta=eta, wInit=("uniform", wlb, wub), w_bound=0.,
                                  optim_type=optim_type, signVal=-1., key=subkeys[6])
         ## set up feedback/error synapses
-        E1 = model.add_component("hebbian", name="E2", shape=(hid2_dim, hid1_dim),
-                                 eta=0., wInit=("uniform", -0.3, 0.3), w_bound=0.,
+        E2 = model.add_component("hebbian", name="E2", shape=(hid2_dim, hid1_dim),
+                                 eta=0., wInit=("uniform", wlb, wub), w_bound=0.,
                                  signVal=-1., key=subkeys[4])
         E3 = model.add_component("hebbian", name="E3", shape=(out_dim, hid2_dim),
-                                 eta=0., wInit=("uniform", -0.3, 0.3), w_bound=0.,
+                                 eta=0., wInit=("uniform", wlb, wub), w_bound=0.,
                                  signVal=-1., key=subkeys[5])
         ## wire z0 to e1.mu via W1
         model.connect(z0.name, z0.outputCompartmentName(), W1.name, W1.inputCompartmentName())
@@ -82,15 +90,41 @@ class DC_SNN_Model():
         model.connect(z2.name, z2.outputCompartmentName(), W3.name, W3.presynapticCompartmentName())
         model.connect(e3.name, e3.derivMeanName(), W3.name, W3.postsynapticCompartmentName())
 
+        ## construct inference / projection model
+        q0 = model.add_component("rate", name="q0", n_units=in_dim, tau_m=0., act_fx="identity")
+        q1 = model.add_component("rate", name="q1", n_units=hid1_dim, tau_m=0., act_fx=act_fx)
+        q2 = model.add_component("rate", name="q2", n_units=hid2_dim, tau_m=0., act_fx=act_fx)
+        q3 = model.add_component("rate", name="q3", n_units=out_dim, tau_m=0., act_fx="identity")
+        eq3 = model.add_component("error", name="eq3", n_units=out_dim)
+        Q1 = model.add_component("hebbian", name="Q1", shape=(in_dim, hid1_dim), key=subkeys[0])
+        Q2 = model.add_component("hebbian", name="Q2", shape=(hid1_dim, hid2_dim), key=subkeys[0])
+        Q3 = model.add_component("hebbian", name="Q3", shape=(hid2_dim, out_dim), key=subkeys[0])
+        ## wire q0 -(Q1)-> q1, q1 -(Q2)-> q2, q2 -(Q3)-> q3
+        model.connect(q0.name, q0.outputCompartmentName(), Q1.name, Q1.inputCompartmentName())
+        model.connect(Q1.name, Q1.outputCompartmentName(), q1.name, q1.inputCompartmentName())
+        model.connect(q1.name, q1.outputCompartmentName(), Q2.name, Q2.inputCompartmentName())
+        model.connect(Q2.name, Q2.outputCompartmentName(), q2.name, q2.inputCompartmentName())
+        model.connect(q2.name, q2.outputCompartmentName(), Q3.name, Q3.inputCompartmentName())
+        model.connect(Q3.name, Q3.outputCompartmentName(), q3.name, q3.inputCompartmentName())
+        ## wire q3 to qe3
+        model.connect(q3.name, q3.rateActivityName(), eq3.name, eq3.targetName())
+
         ## checks that everything is valid within model structure
         #model.verify_cycle()
 
         ## make key commands known to model
         ## will need to clamp to z3 and e3.target = x
         model.add_command("reset", command_name="reset",
-                          component_names=[z0.name, z1.name, z2.name, z3.name,
+                          component_names=[q0.name, q1.name, q2.name, q3.name, eq3.name,
+                                           z0.name, z1.name, z2.name, z3.name,
                                            e1.name, e2.name, e3.name],
                           reset_name="do_reset")
+        model.add_command(
+            "advance", command_name="project",
+            component_names=[q0.name, Q1.name, q1.name, Q2.name,
+                             q2.name, Q3.name, q3.name, eq3.name,
+                            ]
+        )
         model.add_command(
             "advance", command_name="advance",
             component_names=[E2.name, E3.name,
@@ -102,10 +136,14 @@ class DC_SNN_Model():
         model.add_command("evolve", command_name="evolve",
                           component_names=[W1.name, W2.name, W3.name])
         model.add_command("clamp", command_name="clamp_input",
-                          component_names=[z0.name], compartment=z0.inputCompartmentName(),
+                          component_names=[z0.name, q0.name],
+                          compartment=z0.inputCompartmentName(),
                           clamp_name="x")
         model.add_command("clamp", command_name="clamp_target",
-                          component_names=[e0.name], compartment=e0.targetName(),
+                          component_names=[z3.name], compartment=z3.inputCompartmentName(),
+                          clamp_name="target")
+        model.add_command("clamp", command_name="clamp_infer_target",
+                          component_names=[eq3.name], compartment=eq3.targetName(),
                           clamp_name="target")
         model.add_command("save", command_name="save",
                           component_names=[W1.name, W2.name, W3.name],
@@ -121,33 +159,40 @@ class DC_SNN_Model():
         model.save(dir=self.model_dir) ## save current parameter arrays
         self.circuit = model # embed model construct to agent "circuit"
 
-        ## TODO: port over CSDP option to plug in optimizer into Hebb synapse
-
     def process(self, obs, lab, adapt_synapses=True):
-        ## run analytical model
-        #W1 = model.components["W1"].weights
-        #W2 = model.components["W2"].weights
-        #z0_mu, z1_mu, z1_f, z2_f, e0, e1, dW1, dW2 = calc(Xb, W1, W2, K=T, beta=dt/tau_m)
-        # print("+++++++++++++++++ Ref ++++++++++++++++++++++")
-        # print("z2: ",z2_f)
-        # print("mu1: ",z1_mu)
-        # print("e1: ",e1)
-        # print("z1: ",z1_f)
-        # print("mu0: ",z0_mu)
-        # print("e0: ",e0)
-        # print("++++++++++++++++++++++++++++++++++++++++++++")
-
         self.circuit.reset(do_reset=True)
+        ## pin inference synapses to be exactly equal to the forward ones
+        self.circuit.components["Q1"].weights = (self.circuit.components["W1"].weights)
+        self.circuit.components["Q2"].weights = (self.circuit.components["W2"].weights)
+        self.circuit.components["Q3"].weights = (self.circuit.components["W3"].weights)
         ## pin feedback synapses to transpose of forward ones
         self.circuit.components["E2"].weights = (self.circuit.components["W2"].weights).T
         self.circuit.components["E3"].weights = (self.circuit.components["W3"].weights).T
+
+        ## Perform P-step (projection step)
+        self.circuit.clamp_input(x=obs) ## clamp to q0 & z0 input compartments
+        self.circuit.clamp_infer_target(target=lab)
+        self.circuit.project(t=0, dt=0.) ## do projection/inference
+        ## initialize dynamics of generative model latents to projected states
+        self.circuit.components["z1"].compartments["z"] = self.circuit.components["q1"].compartments["z"]
+        self.circuit.components["z2"].compartments["z"] = self.circuit.components["q2"].compartments["z"]
+        ###self.circuit.components["z3"].compartments["z"] = self.circuit.components["q3"].compartments["z"]
+        ## pin projection statistics to main inference components
+        ### Note: e1 = 0, e2 = 0 at initial conditions
+        self.circuit.components["e3"].compartments["dmu"] = self.circuit.components["eq3"].compartments["dmu"]
+        self.circuit.components["e3"].compartments["dtarget"] = self.circuit.components["eq3"].compartments["dtarget"]
+
+        print("mu: ",self.circuit.components["q3"].compartments["z"])
         ## Perform E-step
-        for ts in range(0, T):
+        for ts in range(0, self.T):
             #print("###################### {} #########################".format(ts))
-            self.circuit.clamp_input(x=obs) #x=inp) ## clamp data to z0.input
+            self.circuit.clamp_input(x=obs) ## clamp data to z0 & q0 input compartments
             self.circuit.clamp_target(target=lab) ## clamp data to e3.target
             #print("e0.comp = ",model.components["e0"].compartments)
-            self.circuit.runCycle(t=ts*dt, dt=dt)
+            self.circuit.runCycle(t=ts*self.dt, dt=self.dt)
+        #print("mu: ",self.circuit.components["e3"].compartments["mu"])
+        print(" y: ",self.circuit.components["e3"].compartments["target"])
+        print("---")
         ## Perform (optional) M-step (scheduled synaptic updates)
         if adapt_synapses == True:
-            self.circuit.evolve(t=T, dt=dt)
+            self.circuit.evolve(t=self.T, dt=self.dt)
---
+++ exhibits/pc_discrim/train_model.py
@@ -0,0 +1,22 @@
+#from ngcsimlib.controller import Controller
+from jax import numpy as jnp, random, nn, jit
+import sys
+from pc_model import PCN
+
+
+dkey = random.PRNGKey(1234)
+dkey, *subkeys = random.split(dkey, 10)
+
+n_iter = 100
+
+in_dim = 3
+Xb = random.uniform(subkeys[0], (1, in_dim), dtype=jnp.float32)
+out_dim = 2
+Yb = random.uniform(subkeys[0], (1, out_dim), minval=0., maxval=0.6, dtype=jnp.float32)
+Yb = Yb * jnp.asarray([[-1, 1.]])
+
+model = PCN(subkeys[1], in_dim, out_dim, hid1_dim=16, hid2_dim=16, T=20,
+            dt=1., tau_m=20., act_fx="lrelu", exp_dir="exp", model_name="pcn")
+
+for i in range(n_iter):
+    model.process(obs=Xb, lab=Yb, adapt_synapses=True)
