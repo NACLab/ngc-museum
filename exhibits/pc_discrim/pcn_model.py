067f196936ab53b7588b70ad20b0ee836b32e109
ago109 <ago109@ist.psu.edu>
2024-05-26 18:08:57 -0400

    first draft of pcn rewrite

--- exhibits/pc_discrim/pcn_model.py
+++ exhibits/pc_discrim/pcn_model.py
@@ -312,7 +312,7 @@ class PCN():
             self.q3.advance_state(t, dt)
             self.eq3.advance_state(t, dt)
 
-        self.circuit.add_command(jit(self.project), name="project")
+        self.circuit.add_command(jit(self.circuit.project), name="project")
 
         # @scanner
         # def process(compartment_values, args):
