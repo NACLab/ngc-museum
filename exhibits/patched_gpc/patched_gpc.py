6e69f94fa6daca9e4a3b38fc404461f0340a7c10
Faezeh Habibi <155960330+Faezehabibi@users.noreply.github.com>
2024-08-06 23:57:52 -0400

    Update patched_gpc.py

--- exhibits/patched_gpc/patched_gpc.py
+++ exhibits/patched_gpc/patched_gpc.py
@@ -8,9 +8,9 @@ from ngclearn import numpy as jnp
 from ngclearn.utils.model_utils import normalize_matrix
 import numpy as np
 
-from ngclearn.components import (RateCell, GaussianErrorCell as ErrorCell)
+from ngclearn.components import GaussianErrorCell as ErrorCell
+from ngclearn.components import RateCell
 from ngclearn.components.synapses import HebbianPatchedSynapse, StaticPatchedSynapse
-
 
 
 class HierarchicalPatching_GPC():
@@ -48,68 +48,77 @@ class HierarchicalPatching_GPC():
         else:
             with Context("Circuit") as self.circuit:
                 # layer 2 neurons
-                z2 = RateCell("z2", n_units=D2, tau_m=self.tau_m2,
+                self.z2 = RateCell("z2", n_units=D2, tau_m=self.tau_m2,
                                            resist_scale=resist_scale2,
                                            prior=((z2_prior_type, z2_lmbda_prior)),
-                                           act_fx=act_fx2, batch_size=batch_size
+                                           act_fx=act_fx2
                               )
                 # layer 1 neurons
-                e1 = ErrorCell("e1", n_units=D1, batch_size=batch_size)
-                z1 = RateCell("z1", n_units=D1, tau_m=self.tau_m1,
+                self.e1 = ErrorCell("e1", n_units=D1)
+                self.z1 = RateCell("z1", n_units=D1, tau_m=self.tau_m1,
                                            prior=((z1_prior_type, z1_lmbda_prior)),
                                            resist_scale=resist_scale1,
-                                           act_fx=act_fx1, batch_size=batch_size
+                                           act_fx=act_fx1
                               )
                 # input layer
-                e0 = ErrorCell("e0", n_units=D0, batch_size=batch_size)
+                self.e0 = ErrorCell("e0", n_units=D0)
 
                 # layer 2 synapses
-                W2 = HebbianPatchedSynapse("W2", shape=(D2, D1), n_sub_models=n2,
+                self.W2 = HebbianPatchedSynapse("W2", shape=(D2, D1), n_sub_models=n2,  
                                                         eta=self.eta2, w_decay=w_decay2,
                                                         pre_wght=pre_wght2, weight_init=weight2_init, bias_init=self.bias_init,
                                                         optim_type=optim_type2, sign_value=sign_value2, w_bound=w_bound2, key=subkeys[3]
                                            )
-                E2 = StaticPatchedSynapse("E2", shape=(D1, D2), n_sub_models=n2,
+                self.E2 = StaticPatchedSynapse("E2", shape=(D1, D2), n_sub_models=n2,  
                                                        weight_init=weight2_init, bias_init=self.bias_init, key=subkeys[4])
                 # layer 1 synapses
-                W1 = HebbianPatchedSynapse("W1", shape=(D1, D0), n_sub_models=n1,
+                self.W1 = HebbianPatchedSynapse("W1", shape=(D1, D0), n_sub_models=n1,   
                                                         eta=self.eta1, w_decay=w_decay1,
                                                         pre_wght=pre_wght1, weight_init=weight1_init, bias_init=self.bias_init,
                                                         optim_type=optim_type1, sign_value=sign_value1, w_bound=w_bound1, key=subkeys[5]
                                            )
-                E1 = StaticPatchedSynapse("E1", shape=(D0, D1), n_sub_models=n1,
+                self.E1 = StaticPatchedSynapse("E1", shape=(D0, D1), n_sub_models=n1,  
                                                        weight_init=weight1_init, bias_init=self.bias_init, key=subkeys[6])
 
-                W2.inputs << z2.zF
-                e1.mu << W2.outputs
-                E2.inputs << e1.dmu
-                z2.j << E2.outputs
+                self.e0.batch_size = batch_size
+                self.e1.batch_size = batch_size
+                self.z1.batch_size = batch_size
+                self.z2.batch_size = batch_size
+                self.W1.batch_size = batch_size
+                self.W2.batch_size = batch_size
+                self.E1.batch_size = batch_size
+                self.E2.batch_size = batch_size
 
-                z1.j_td << e1.dtarget
+                self.W2.inputs << self.z2.zF
+                self.e1.mu << self.W2.outputs
+                self.E2.inputs << self.e1.dmu
+                self.z2.j << self.E2.outputs
 
-                W1.inputs << z1.zF
-                e0.mu << W1.outputs
-                E1.inputs << e0.dmu
-                z1.j << E1.outputs
+                self.z1.j_td << self.e1.dtarget
 
-                e1.target << z1.z
+                self.W1.inputs << self.z1.zF
+                self.e0.mu << self.W1.outputs
+                self.E1.inputs << self.e0.dmu
+                self.z1.j << self.E1.outputs
 
-                W1.pre << z1.zF
-                W1.post << e0.dmu
-                W2.pre << z2.zF
-                W2.post << e1.dmu
+                self.e1.target << self.z1.z
 
-                advance_cmd, advance_args = self.circuit.compile_by_key( E1, E2, ## execute feedback first
-                                                                                     z2, z1, ## execute state neurons
-                                                                                     W2, W1, ## execute prediction synapses
-                                                                                    e1, e0, ## finally, execute error neurons
+                self.W1.pre << self.z1.zF
+                self.W1.post << self.e0.dmu
+                self.W2.pre << self.z2.zF
+                self.W2.post << self.e1.dmu
+
+                advance_cmd, advance_args = self.circuit.compile_by_key( self.E1, self.E2, ## execute feedback first
+                                                                                     self.z2, self.z1, ## execute state neurons
+                                                                                     self.W2, self.W1, ## execute prediction synapses
+                                                                                    self.e1, self.e0, ## finally, execute error neurons
                                                                            compile_key="advance_state")
 
-                evolve_cmd, evolve_args = self.circuit.compile_by_key(W1, W2,
+                evolve_cmd, evolve_args = self.circuit.compile_by_key(self.W1, self.W2,
                                                                       compile_key="evolve")
 
-                reset_cmd, reset_args = self.circuit.compile_by_key(z1, z2, e0, e1,
-                                                                                W1, W2, E1, E2,
+                reset_cmd, reset_args = self.circuit.compile_by_key(self.z1, self.z2, self.e0, self.e1,
+                                                                                self.W1, self.W2, self.E1, self.E2,
                                                                     compile_key="reset")
 
                 self.dynamic()
@@ -127,12 +136,12 @@ class HierarchicalPatching_GPC():
 
         @Context.dynamicCommand
         def clamp(x):
-            self.e0.target.set(x)
+            e0.target.set(x)
 
         @Context.dynamicCommand
         def norm():
-            self.W1.weights.set(normalize_matrix(self.W1.weights.value, 1., order=2, axis=1))
-            self.W2.weights.set(normalize_matrix(self.W2.weights.value, 1., order=2, axis=1))
+            W1.weights.set(normalize_matrix(W1.weights.value, 1., order=2, axis=1))
+            W2.weights.set(normalize_matrix(W2.weights.value, 1., order=2, axis=1))
 
         self.circuit.add_command(wrap_command(jit(self.circuit.reset)), name="reset")
         self.circuit.add_command(wrap_command(jit(self.circuit.advance_state)), name="advance")
@@ -199,13 +208,23 @@ class HierarchicalPatching_GPC():
 
 
     def process(self, sensory_in, adapt_synapses=True):
-        self.circuit.reset()
+
+        self.e0.batch_size = sensory_in.shape[0]
+        self.e1.batch_size = sensory_in.shape[0]
+        self.z1.batch_size = sensory_in.shape[0]
+        self.z2.batch_size = sensory_in.shape[0]
+        self.W1.batch_size = sensory_in.shape[0]
+        self.W2.batch_size = sensory_in.shape[0]
+        self.E1.batch_size = sensory_in.shape[0]
+        self.E2.batch_size = sensory_in.shape[0]
 
         self.E1.weights.set(jnp.transpose(self.W1.weights.value))
         self.E2.weights.set(jnp.transpose(self.W2.weights.value))
+
+        self.circuit.reset()
         # ########################################################################
         self.circuit.clamp(sensory_in)
-        self.circuit.process(jnp.array([[self.dt * i, self.dt] for i in range(self.T)]))
+        z1_codes = self.circuit.process(jnp.array([[self.dt * i, self.dt] for i in range(self.T)]))
 
         if adapt_synapses is True:
             self.circuit.evolve(t=self.T, dt=1.)
