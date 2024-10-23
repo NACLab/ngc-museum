e481aef00aabc7cfd66fae6570c44a3eeb1a5828
Faezeh Habibi <155960330+Faezehabibi@users.noreply.github.com>
2024-10-23 17:45:00 -0400

    Add files via upload

---
+++ exhibits/sindy/sindy.py
@@ -0,0 +1,86 @@
+import pandas as pd
+from jax import random, jit
+import matplotlib.pyplot as plt
+import numpy as np
+from ngclearn.utils import weight_distribution as dist
+from ngclearn import Context, numpy as jnp
+from ngclearn.utils.model_utils import scanner
+from ngclearn.components import (HebbianSynapse,
+                                 GaussianErrorCell)
+from typing import Optional
+from ngcsimlib.compilers import compile_command, wrap_command
+from functools import partial
+from ngclearn.utils.diffeq.feature_library import CreateLibrary
+
+
+class Std_SINDy():
+    '''
+    STLSQ (Sequential Thresholded Least Squares)
+    '''
+    def __init__(self, threshold=0.5, max_iter=20):
+        self.library_type: str = 'polynomial'
+        self.polynomial_order: int = 2
+        self.threshold: float = threshold
+        self.max_iter: int = max_iter
+        self.coef_:Optional[jnp.ndarray] = None
+        self.lib_:Optional[jnp.ndarray] = None
+
+    def fit(self, dx: jnp.ndarray, lib: jnp.ndarray) -> jnp.ndarray:
+        # Solve for coef that gives min ||lib @ coef - dx||^2
+        coef = jnp.linalg.lstsq(lib, dx, rcond=None)[0]
+
+        for _ in range(self.max_iter):
+            coef_pre = coef.copy()
+
+            coef_zero = jnp.zeros_like(coef)
+
+            res_idx = jnp.where(jnp.abs(coef) >= self.threshold, True, False)
+            res_mask = jnp.any(res_idx, axis=1)
+            res_lib = lib[:, res_mask]
+
+            # repeating least square with residual features in libreary
+            coef_new = jnp.linalg.lstsq(res_lib, dx, rcond=None)[0]
+            sparse_coef = coef_zero.at[res_mask].set(coef_new)
+
+            # remove non-sharing coefficients for each dims in feature librate
+            coef = jnp.where(jnp.abs(sparse_coef) >= self.threshold, coef, 0.)
+
+            # convergence check
+            if jnp.allclose(coef, coef_pre, rtol=1e-6, atol=1e-6):
+                print('converget at iteration = {}'.format(_))
+                break
+
+        self.coef_ = coef
+        self.lib_ = lib
+        return coef
+
+    def predict(self) -> jnp.ndarray:
+        '''
+            Predict time derivatives using the learned SINDy model.
+
+        Returns
+        -------
+        dx_pred : jnp.ndarray
+        '''
+
+        return self.lib_ @ self.coef_
+
+    def get_ode(self, feature_names):
+
+        # print coeff and plot
+        c_bool = jnp.where(self.coef_ == 0, False, True)
+        idx_ = jnp.any(c_bool == True, axis=1)
+        c_ = self.coef_[idx_]
+        n_ = [res_trm for res_trm, i_ in zip(feature_names, idx_) if i_]
+
+        if self.coef_.shape[1] == 3:
+            df_ = pd.DataFrame(jnp.round(c_, 3), index=n_, columns=['ẋ', 'ẏ', 'ż'])
+
+        elif self.coef_.shape[1] == 2:
+            df_ = pd.DataFrame(jnp.round(c_, 3), index=n_, columns=['ẋ', 'ẏ'])
+
+        model_ode = df_
+        return model_ode
+
+
+
