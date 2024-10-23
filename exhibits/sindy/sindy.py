c8acc2a994ed2fe7ac72667516cc44419e0e1c4e
Faezeh Habibi <155960330+Faezehabibi@users.noreply.github.com>
2024-10-22 22:02:06 -0400

    Update sindy.py

--- exhibits/sindy/sindy.py
+++ exhibits/sindy/sindy.py
@@ -1,89 +1,76 @@
 import pandas as pd
-from jax import random, jit
-import matplotlib.pyplot as plt
-import numpy as np
 from ngclearn import Context, numpy as jnp
-from sindy import Std_SINDy
-from ngclearn.utils.diffeq.feature_library import CreateLibrary
-from ngclearn.utils.diffeq.ode_utils_scanner import solve_ode
-from ngclearn.utils.diffeq.odes import (linear_2D, cubic_2D,
-                                        linear_3D, lorenz)
-
-dfx = cubic_2D
-include_bias = False
-
-if dfx == linear_2D:
-    x0 = jnp.array([3, -1.5])
-    deg = 2
-    threshold = 0.01
-    T = 2000
-    include_bias = False
-elif dfx == cubic_2D:
-    x0 = jnp.array([2., 0.])
-    deg = 3
-    threshold = 0.01
-    T = 1000
-    include_bias = False
-elif dfx == linear_3D:
-    x0 = jnp.array([1, 1., -1])
-    deg = 2
-    threshold = 0.001
-    T = 2000
-    include_bias = False
-elif dfx == lorenz:
-    x0 = jnp.array([-8, 8, 27], dtype=jnp.float32)
-    threshold = 0.1
-    deg = 2
-    T = 500
-    include_bias = False
+from typing import Optional
 
 
+class Std_SINDy():
+    '''
+    STLSQ (Sequential Thresholded Least Squares)
+    '''
+    def __init__(self, threshold=0.5, max_iter=20):
+        self.library_type: str = 'polynomial'
+        self.polynomial_order: int = 2
+        self.threshold: float = threshold
+        self.max_iter: int = max_iter
+        self.coef_:Optional[jnp.ndarray] = None
+        self.lib_:Optional[jnp.ndarray] = None
 
-dt = 1e-2
-t0 = 0.
-ts, X = solve_ode('rk4', t0, x0, T=T, dfx=dfx, dt=dt, params=None, sols_only=True)
-# -------------------------------------------Numerical derivate calculation---------------------------------------------
-dX = jnp.array(np.gradient(jnp.array(X), ts.ravel(), axis=0))
-# ------------------------------------------------codes---------------------------------------------
-lib_creator = CreateLibrary(poly_order=deg, include_bias=include_bias)
-feature_lib, feature_names = lib_creator.fit([X[:, i] for i in range(X.shape[1])])
+    def fit(self, dx: jnp.ndarray, lib: jnp.ndarray) -> jnp.ndarray:
+        # Solve for coef that gives min ||lib @ coef - dx||^2
+        coef = jnp.linalg.lstsq(lib, dx, rcond=None)[0]
 
-model = Std_SINDy(threshold=0.1, max_iter=20)
-print('---------- std-sindy coefficients ----------')
-sparse_coef = model.fit(dx=dX, lib=feature_lib)
-pred = model.predict() #feature_lib @ sparse_coef
+        for _ in range(self.max_iter):
+            coef_pre = coef.copy()
 
-ode_ = model.get_ode(feature_names)
-c = jnp.where(sparse_coef==0, False, True)
-idx_ = jnp.any(c == True, axis=1)
-c_ = sparse_coef[idx_]
-n_ = [name_ for name_, i_ in zip(feature_names, idx_) if i_]
+            coef_zero = jnp.zeros_like(coef)
 
-plt.figure(facecolor='floralwhite')
+            res_idx = jnp.where(jnp.abs(coef) >= self.threshold, True, False)
+            res_mask = jnp.any(res_idx, axis=1)
+            res_lib = lib[:, res_mask]
 
-if sparse_coef.shape[1]==3:
-    df = pd.DataFrame(jnp.round(sparse_coef, 3), index=feature_names, columns=['ẋ', 'ẏ', 'ż'])
-    plt.plot(ts, dX[:, 0], label=r'$\dot{x}$', linewidth=5, alpha=0.3, color='turquoise')
-    plt.plot(ts, pred[:, 0], label=r'$\hat{\dot{x}}$', linewidth=0.8, ls="--", color='black')
-    plt.plot(ts, dX[:, 1], label=r'$\dot{y}$', linewidth=4, alpha=0.6, color='pink')
-    plt.plot(ts, pred[:, 1], label=r'$\hat{\dot{y}}$', linewidth=0.8, ls='--', color='red')
-    plt.plot(ts, dX[:, 2], label=r'$\dot{z}$', linewidth=2, alpha=0.8, color='yellow')
-    plt.plot(ts, pred[:, 2], label=r'$\hat{\dot{z}}$', linewidth=0.8, ls="--", color='navy')
+            # repeating least square with residual features in libreary
+            coef_new = jnp.linalg.lstsq(res_lib, dx, rcond=None)[0]
+            sparse_coef = coef_zero.at[res_mask].set(coef_new)
 
-elif sparse_coef.shape[1]==2:
-    df = pd.DataFrame(jnp.round(sparse_coef, 3), index=feature_names, columns=['ẋ', 'ẏ'])
-    plt.plot(ts, dX[:, 0], label=r'$\dot{x}$', linewidth=5, alpha=0.3, color='turquoise')
-    plt.plot(ts, pred[:, 0], label=r'$\hat{\dot{x}}$', linewidth=0.8, ls="--", color='black')
-    plt.plot(ts, dX[:, 1], label=r'$\dot{y}$', linewidth=4, alpha=0.6, color='pink')
-    plt.plot(ts, pred[:, 1], label=r'$\hat{\dot{y}}$', linewidth=0.8, ls='--', color='red')
+            # remove non-sharing coefficients for each dims in feature librate
+            coef = jnp.where(jnp.abs(sparse_coef) >= self.threshold, coef, 0.)
 
-print(ode_)
+            # convergence check
+            if jnp.allclose(coef, coef_pre, rtol=1e-6, atol=1e-6):
+                print('converget at iteration = {}'.format(_))
+                break
 
-plt.grid(True)
-plt.legend(loc='lower right')
-plt.xlabel(r'$time$', fontsize=10)
-plt.ylabel(r'$\{\dot{x}, \dot{y}, \dot{z}\}$', fontsize=8)
+        self.coef_ = coef
+        self.lib_ = lib
+        return coef
 
-plt.title('Sparse Coefficients of {} model'.format(str(dfx.__name__)))
-# plt.title(f'{ode_}', fontsize=7)
-plt.show()
+    def predict(self) -> jnp.ndarray:
+        '''
+            Predict time derivatives using the learned SINDy model.
+
+        Returns
+        -------
+        dx_pred : jnp.ndarray
+        '''
+
+        return self.lib_ @ self.coef_
+
+    def get_ode(self, feature_names):
+
+        # print coeff and plot
+        c_bool = jnp.where(self.coef_ == 0, False, True)
+        idx_ = jnp.any(c_bool == True, axis=1)
+        c_ = self.coef_[idx_]
+        n_ = [res_trm for res_trm, i_ in zip(feature_names, idx_) if i_]
+
+        if self.coef_.shape[1] == 3:
+            df_ = pd.DataFrame(jnp.round(c_, 3), index=n_, columns=['ẋ', 'ẏ', 'ż'])
+
+        elif self.coef_.shape[1] == 2:
+            df_ = pd.DataFrame(jnp.round(c_, 3), index=n_, columns=['ẋ', 'ẏ'])
+
+        model_ode = df_
+        return model_ode
+
+
+
