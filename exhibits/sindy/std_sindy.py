43537fd7ff5a0471a4903fdb89f450c244106288
Alex Ororbia <agocse109@gmail.com>
2024-12-09 22:56:19 -0500

    Update std_sindy.py
    Ported over and revised code from ngc-museum-faeze

--- exhibits/sindy/std_sindy.py
+++ exhibits/sindy/std_sindy.py
@@ -1,12 +1,16 @@
 import pandas as pd
 from ngclearn import Context, numpy as jnp
 from typing import Optional
-import matplotlib.pyplot as plt
 
 
 class Std_SINDy():
     '''
-    STLSQ (Sequential Thresholded Least Squares)
+    An ngc-learn implementation/reproduction of the Sparse Identification of Non-linear Dynamical systems 
+    (SINDy) model proposed in: 
+
+    Brunton, Steven L., Joshua L. Proctor, and J. Nathan Kutz. "Discovering governing equations 
+    from data by sparse identification of nonlinear dynamical systems." Proceedings of the 
+    national academy of sciences 113.15 (2016): 3932-3937.
     '''
     def __init__(self, threshold=0.5, max_iter=20):
         self.library_type: str = 'polynomial'
@@ -16,82 +20,70 @@ class Std_SINDy():
         self.coef_:Optional[jnp.ndarray] = None
         self.lib_:Optional[jnp.ndarray] = None
 
-
     def fit(self, dx: jnp.ndarray, lib: jnp.ndarray) -> jnp.ndarray:
         # Solve for coef that gives min ||lib @ coef - dx||^2
         coef = jnp.linalg.lstsq(lib, dx, rcond=None)[0]
 
-        for iter_ in range(self.max_iter):
-            coef_pre = jnp.array(coef)
+        for _ in range(self.max_iter):
+            coef_pre = coef.copy()
+
             coef_zero = jnp.zeros_like(coef)
 
             res_idx = jnp.where(jnp.abs(coef) >= self.threshold, True, False)
             res_mask = jnp.any(res_idx, axis=1)
             res_lib = lib[:, res_mask]
 
+            # repeating least square with residual features in libreary
             coef_new = jnp.linalg.lstsq(res_lib, dx, rcond=None)[0]
             sparse_coef = coef_zero.at[res_mask].set(coef_new)
 
-            coef = jnp.where(jnp.abs(sparse_coef) >= self.threshold, sparse_coef, 0.)
+            # remove non-sharing coefficients for each dims in feature librate
+            coef = jnp.where(jnp.abs(sparse_coef) >= self.threshold, coef, 0.)
 
-            if jnp.allclose(sparse_coef, coef_pre, rtol=1e-4, atol=1e-4):
-                # print('converget at iteration = {}'.format(iter_))
-                break
+            # convergence check
+            if jnp.allclose(coef, coef_pre, rtol=1e-6, atol=1e-6):
+                print('Model converged at iteration = {}'.format(_))
+                break ## engage early stopping if condition met
 
         self.coef_ = coef
         self.lib_ = lib
+        self.dx_ = dx
         return coef
 
     def predict(self) -> jnp.ndarray:
         '''
-            Predict time derivatives using the learned SINDy model.
+            Predict the time derivatives using the learned SINDy model.
 
-        Returns
-        -------
-        dx_pred : jnp.ndarray
+            Returns:
+                dx_pred : jnp.ndarray
         '''
 
         return self.lib_ @ self.coef_
-        
+
 
     def error(self) -> jnp.ndarray:
         '''
-        Frobenius norm for prediction and taget difference
+            Frobenius norm for prediction and target difference
 
-        :return:
+            Returns:
+                error (between model prediction and correct derivative)
         '''
-        
+
         return jnp.linalg.norm(self.predict() - self.dx_)
 
     def get_ode(self, feature_names):
+
+        # print coeff and plot
         c_bool = jnp.where(self.coef_ == 0, False, True)
         idx_ = jnp.any(c_bool == True, axis=1)
-        c_ = list(self.coef_[idx_])
+        c_ = self.coef_[idx_]
         n_ = [res_trm for res_trm, i_ in zip(feature_names, idx_) if i_]
-        visual_eq = [f"{c_[i][0]:.3f} " + n_[i] + (' +' if (i != len(c_)-1) and (c_[i+1][0]>0) else ' ') for i in range(len(c_))]
 
-        return visual_eq
+        if self.coef_.shape[1] == 3:
+            df_ = pd.DataFrame(jnp.round(c_, 3), index=n_, columns=['ẋ', 'ẏ', 'ż'])
 
-    def vis_sys(self, ts, dX, pred, model):
+        elif self.coef_.shape[1] == 2:
+            df_ = pd.DataFrame(jnp.round(c_, 3), index=n_, columns=['ẋ', 'ẏ'])
 
-        plt.figure(facecolor='floralwhite')
-
-        plt.plot(ts, dX[:, 0], label=r'$\dot{x}$', linewidth=5, alpha=0.3, color='turquoise')
-        plt.plot(ts, pred[:, 0], label=r'$\hat{\dot{x}}$', linewidth=0.8, ls="--", color='black')
-
-        if dX.shape[1] >= 2:
-            plt.plot(ts, dX[:, 1], label=r'$\dot{y}$', linewidth=4, alpha=0.6, color='pink')
-            plt.plot(ts, pred[:, 1], label=r'$\hat{\dot{y}}$', linewidth=0.8, ls='--', color='red')
-
-        if dX.shape[1] >= 3:
-            plt.plot(ts, dX[:, 2], label=r'$\dot{z}$', linewidth=2, alpha=0.8, color='yellow')
-            plt.plot(ts, pred[:, 2], label=r'$\hat{\dot{z}}$', linewidth=0.8, ls="--", color='navy')
-
-        plt.grid(True)
-        plt.legend(loc='lower right')
-        plt.xlabel(r'$time$', fontsize=10)
-        plt.ylabel(r'$\{\dot{x}, \dot{y}, \dot{z}\}$', fontsize=8)
-
-        plt.title('Sparse Coefficients of "{}" model'.format(model.__name__))
-        plt.show()
-
+        model_ode = df_
+        return model_ode
