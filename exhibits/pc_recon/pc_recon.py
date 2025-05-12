Fix: test reconstruction loss calculation (#19)
* fix: loss calculation

* refactor: synaptic visualization and naming

exhibits/pc_recon/pc_recon.py
@@ -8,8 +8,8 @@ from ngclearn.utils.model_utils import scanner
 from ngcsimlib.context import Context
 from ngclearn.utils import JaxProcess
 from ngcsimlib.compilers import wrap_command, compile_command
-from ngclearn.components import (RateCell, HebbianPatchedSynapse as HebbianSynapse,
-                                 GaussianErrorCell, StaticPatchedSynapse as StaticSynapse)
+from ngclearn.components import (RateCell, HebbianPatchedSynapse as BackwardSynapse,
+                                 GaussianErrorCell, StaticPatchedSynapse as ForwardSynapse)
 from sklearn.feature_extraction.image import extract_patches_2d, reconstruct_from_patches_2d
 
 import ngclearn.utils.weight_distribution as dist
@@ -124,7 +124,7 @@ class PC_Recon():
                 self.z2 = RateCell("z2", n_units=h2_dim, tau_m=tau_m, act_fx=act_fx, prior=(prior_type, lmbda))
                 self.z1 = RateCell("z1", n_units=h1_dim, tau_m=tau_m, act_fx=act_fx, prior=(prior_type, lmbda))
 
-                self.W3 = HebbianSynapse("W3",
+                self.W3 = BackwardSynapse("W3",
                                          shape=(h3_dim, h2_dim),
                                          n_sub_models=self.n_p3,     ## number of modules in the layer
                                          sign_value=-1.,                  ## -1 means M-step solve minimization problem
@@ -134,7 +134,7 @@ class PC_Recon():
                                          w_bound=w_bound,
                                          key=subkeys[2]
                                          )
-                self.W2 = HebbianSynapse("W2",
+                self.W2 = BackwardSynapse("W2",
                                          shape=(h2_dim, h1_dim),
                                          n_sub_models=self.n_p2,     ## number of modules in the layer
                                          sign_value=-1.,                  ## -1 means M-step solve minimization problem
@@ -144,7 +144,7 @@ class PC_Recon():
                                          w_bound=w_bound,
                                          key=subkeys[1]
                                          )
-                self.W1 = HebbianSynapse("W1", shape=(h1_dim, in_dim),
+                self.W1 = BackwardSynapse("W1", shape=(h1_dim, in_dim),
                                          n_sub_models=n_p1,              ## number of modules in the layer
                                          sign_value=-1.,                 ## -1 means M-step solve minimization problem
                                          optim_type=opt_type,
@@ -158,9 +158,9 @@ class PC_Recon():
                 self.e1 = GaussianErrorCell("e1", n_units=h1_dim)
                 self.e0 = GaussianErrorCell("e0", n_units=in_dim)
 
-                self.E3 = StaticSynapse("E3", shape=(h2_dim, h3_dim), n_sub_models=self.n_p3, key=subkeys[2])
-                self.E2 = StaticSynapse("E2", shape=(h1_dim, h2_dim), n_sub_models=self.n_p2, key=subkeys[1])
-                self.E1 = StaticSynapse("E1", shape=(in_dim, h1_dim), n_sub_models=self.n_p1, key=subkeys[0])
+                self.E3 = ForwardSynapse("E3", shape=(h2_dim, h3_dim), n_sub_models=self.n_p3, key=subkeys[2])
+                self.E2 = ForwardSynapse("E2", shape=(h1_dim, h2_dim), n_sub_models=self.n_p2, key=subkeys[1])
+                self.E1 = ForwardSynapse("E1", shape=(in_dim, h1_dim), n_sub_models=self.n_p1, key=subkeys[0])
 
                 ############################################################
                 ## since this model will operate with batches, we need to
@@ -388,8 +388,10 @@ class PC_Recon():
 
             module_idx = 0
             rf_vis = h1_rf[module_idx, :, :].reshape(-1, self.inPatch_dim)
-            visualize([rf_vis[:max_filter, :].T],
+            rnd_idx = np.random.randint(0, len(rf_vis), max_filter)
+            visualize([rf_vis[rnd_idx, :].T],
                       [patch_shape], prefix=self.exp_dir + "/filters/{}".format(fname))
+
 
 
             if stride_shape!=(0, 0):

exhibits/pc_recon/train_pc_recon.py
@@ -111,7 +111,6 @@ else:  # Patch case
     images_per_batch = 1
 
 
-
 print("Network Dimensions: {} >> {} >> {} >> {}".format(h3_dim, h2_dim, h1_dim, in_dim))
 print("Network Dimensions: {}{} >> {}{} >> {}{} >> {}{}".format(
                                                 n_p3, p3_size,
@@ -119,6 +118,7 @@ print("Network Dimensions: {}{} >> {}{} >> {}{} >> {}{}".format(
                                                 n_p1, p1_size,
                                                 n_inPatch, pin_size,
 ))
+
 
 print("    mini-batch size : ", mb_size)
 print("    number of images per mini-batch: {}".format(mb_size))
@@ -182,8 +182,8 @@ for i in range(n_iter):
             Xt_mu, L_test = model.process(Xb_test.reshape(mb_size, -1), adapt_synapses=False)
             Xtest_mu = Xt_mu.reshape(mb_size * n_inPatch, -1) + x_test_mean
             ###############################################
-            print("\r >  Test Recon Loss = {} ".format(L_test / mb_size))
-            model.viz_recons(Xtest, Xtest_mu, image_shape=image_shape, fname=f"recons_t{(i % iter_mod) + 1}")
+            print("\r >  Test Recon Loss = {} ".format(L_test / mb_vis_size))
+            model.viz_recons(Xtest, Xtest_mu, image_shape=image_shape, fname=f"recons_t{i+1}")
 
         ## for patched inputs
         elif images_per_batch == 1:
@@ -197,11 +197,12 @@ for i in range(n_iter):
                 Xt_mu, L_test = model.process(Xb_test.reshape(mb_size, -1), adapt_synapses=False)
                 l0 = l0 + L_test
                 ## reconstruct images from patches
-                X_mu = reconstruct_from_patches_2d(np.asarray(Xt_mu).reshape(-1, *patch_shape), image_shape)
+                Xtest_mu = Xt_mu.reshape(mb_size * n_inPatch, -1) + x_test_mean
+                X_mu = reconstruct_from_patches_2d(np.asarray(Xtest_mu).reshape(-1, *patch_shape), image_shape)
                 Xmu_list.append(X_mu.reshape(image_shape[0] * image_shape[1]))
             ###############################################
-            print("\r  >  Test Recon Loss = {} ".format(l0 / mb_size))
-            model.viz_recons(Xtest, np.asarray(Xmu_list), image_shape=image_shape, fname=f"recons_t{(i % iter_mod) + 1}")
+            print("\r  >  Test Recon Loss = {} ".format(l0 / (mb_vis_size * mb_size)))
+            model.viz_recons(Xtest, np.asarray(Xmu_list), image_shape=image_shape, fname=f"recons_t{i+1}")
 
         ###############################################
         model.save_to_disk(params_only=True)                                    ## save final state of synapses to disk
@@ -209,7 +210,7 @@ for i in range(n_iter):
     print()
     if i % viz_mod == 0:
         print(model.get_synapse_stats())
-        model.viz_receptive_fields(patch_shape, max_filter=max_vis_filter, fname=f"erf_t{(i % viz_mod) + 1}")
+        model.viz_receptive_fields(patch_shape, max_filter=max_vis_filter, fname=f"erf_t{i+1}")
 
 ## collect a test sample raster plot
 model.save_to_disk(params_only=True) ## save final model parameters to disk